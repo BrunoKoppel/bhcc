fc02c12ac035d358dae687b4e052bb69fe8b406d
Bruno Koppel <brunokoppel.dev@gmail.com>
2021-03-07 21:23:51 -0500

    Created GUI Design

--- 2021_1Spring/CSC284/BrunoKoppel_Thareos/BrunoKoppel_Thareos_en_US.ts
+++ 2021_1Spring/CSC284/BrunoKoppel_Thareos/BrunoKoppel_Thareos_en_US.ts
@@ -4,8 +4,18 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.ui" line="14"/>
-        <source>MainWindow</source>
+        <location filename="mainwindow.ui" line="21"/>
+        <source>Thareos</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="34"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:28pt;&quot;&gt;Thareos&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="50"/>
+        <source>Login</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
