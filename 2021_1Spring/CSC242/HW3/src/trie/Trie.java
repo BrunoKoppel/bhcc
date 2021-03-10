3a98429c674cdddd24cbd715bc3094138d197a4c
Bruno Koppel <brunokoppel.dev@gmail.com>
2021-03-10 11:32:54 -0500

    adding HW3 and updating gitignore

---
+++ 2021_1Spring/CSC242/HW3/src/trie/Trie.java
@@ -0,0 +1,67 @@
+package trie;
+
+import java.util.Set;
+
+public class Trie {
+
+
+	/**
+	 * Inserts the specified string into the Trie. The Last node associated to the
+	 * of the last char of the specified string will be marked as an end-of-word node.
+	 * @param word
+	 */
+	public void insert(String word){
+
+	}
+
+	/**
+	 * Returns true if this Trie contains the specified string. In other words
+	 * the last char of the specified string is a node that is marked as
+	 * a end-of-word node.
+	 * @param word
+	 * @return
+	 */
+	public boolean contains(String word){
+		return false;
+	}
+
+	/**
+	 * Returns true if this Trie contains the specified string as a word or a
+	 * word in the Trie begins with the specified string.
+	 * @param prefix
+	 * @return
+	 */
+	public boolean containsPrefix(String prefix){
+		return false;
+	}
+
+	/**
+	 * Returns all words that begin with the specified prefix.
+	 * @param prefix
+	 * @return
+	 */
+	public Set<String> getAllWords(String prefix){
+		return null;
+	}
+
+	/**
+	 * Returns all words of odd length (odd number of characters) that begin
+	 * with the specified prefix.
+	 * @param prefix
+	 * @return
+	 */
+	public Set<String> getAllOddWords(String prefix){
+		return null;
+	}
+
+	/**
+	 * Returns all words of even length (even number of characters) that begin
+	 * with the specified prefix.
+	 * @param prefix
+	 * @return
+	 */
+	public Set<String> getAllEvenWords(String prefix){
+		return null;
+	}
+
+}
