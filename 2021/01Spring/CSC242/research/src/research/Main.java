a054768d6d1e3a99ee03d399ef146e76c23d0be7
BrunoKoppel <bruno.koppel@gmail.com>
2021-04-08 10:01:43 -0400

    working on research elements

--- 2021/01Spring/CSC242/research/src/research/Main.java
+++ 2021/01Spring/CSC242/research/src/research/Main.java
@@ -8,14 +8,41 @@ import java.util.*;
 class Main{
     public static void main (String[] args) {
         // define edges of the graph
-        List<Edge> edges = Arrays.asList(new Edge(0, 1, 2),new Edge(0, 2, 4),
-                new Edge(1, 2, 4),new Edge(2, 0, 5), new Edge(2, 1, 4),
-                new Edge(3, 2, 3), new Edge(4, 5, 1),new Edge(5, 4, 3));
+        List<Edge> edges = Arrays.asList(
+                new Edge(0, 1, 2),
+                new Edge(0, 2, 4),
+                new Edge(1, 2, 4),
+                new Edge(2, 0, 5),
+                new Edge(2, 1, 4),
+                new Edge(3, 2, 3),
+                new Edge(4, 5, 1),
+                new Edge(5, 4, 3),
+                new Edge(7, 3, 2));
 
         // call graph class Constructor to construct a graph
         Graph graph = new Graph(edges);
 
         // print the graph as an adjacency list
         Graph.printGraph(graph);
+
+        //create a graph object and add edges to it
+        Graph g = new Graph(5);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(0, 3);
+        g.addEdge(1, 2);
+        g.addEdge(2, 4);
+        //print the DFS Traversal sequence
+        System.out.println("Depth First Traversal for given graph"+
+                "(with 0 as starting vertex)");
+        g.DFS(0);
+    }
+
+    public void DFS(int v) {
+        //initially none of the vertices are visited
+        boolean visited[] = new boolean[Vertices];
+
+        // call recursive DFS_helper function for DFS technique
+        DFS_helper(v, visited);
     }
 }
--- 2021/01Spring/CSC242/research/src/research/classes/Graph.java
+++ 2021/01Spring/CSC242/research/src/research/classes/Graph.java
@@ -4,6 +4,11 @@ import java.util.*;
 
 // Graph class
 public class Graph {
+    private int Vertices;   // No. of vertices
+
+    // adjacency list declaration
+    private LinkedList<Integer> adj_list_int[];
+
     // node of adjacency list
     static class Node {
         int value, weight;
@@ -15,37 +20,64 @@ public class Graph {
 
 // define adjacency list
 
-    List<List<Node>> adj_list = new ArrayList<>();
+    List<List<Node>> adj_list_node = new ArrayList<>();
 
     //Graph Constructor
-    public Graph(List<Edge> edges)
-    {
+    public Graph(List<Edge> edges) {
         // adjacency list memory allocation
         for (int i = 0; i < edges.size(); i++)
-            adj_list.add(i, new ArrayList<>());
+            adj_list_node.add(i, new ArrayList<>());
 
         // add edges to the graph
         for (Edge e : edges)
         {
             // allocate new node in adjacency List from src to dest
-            adj_list.get(e.src).add(new Node(e.dest, e.weight));
+            adj_list_node.get(e.src).add(new Node(e.dest, e.weight));
         }
     }
+
+    public Graph(int v) {
+        Vertices = v;
+        adj_list_int = new LinkedList[v];
+        for (int i=0; i<v; ++i)
+            adj_list_int[i] = new LinkedList();
+    }
+
     // print adjacency list for the graph
     public static void printGraph(Graph graph)  {
         int src_vertex = 0;
-        int list_size = graph.adj_list.size();
+        int list_size = graph.adj_list_node.size();
 
         System.out.println("The contents of the graph:");
         while (src_vertex < list_size) {
             //traverse through the adjacency list and print the edges
-            for (Node edge : graph.adj_list.get(src_vertex)) {
+            for (Node edge : graph.adj_list_node.get(src_vertex)) {
                 System.out.print("Vertex:" + src_vertex + " ==> " + edge.value +
                         " (" + edge.weight + ")\t");
             }
 
             System.out.println();
             src_vertex++;
+        }
+    }
+
+    public void addEdge(int v, int w) {
+        adj_list_int[v].add(w);  // Add w to v's list.
+    }
+
+    // helper function for DFS technique
+    public void DFS_helper(int v,boolean visited[]) {
+        // current node is visited
+        visited[v] = true;
+        System.out.print(v+" ");
+
+        // process all adjacent vertices
+        Iterator<Integer> i = adj_list_int[v].listIterator();
+        while (i.hasNext())
+        {
+            int n = i.next();
+            if (!visited[n])
+                DFS_helper(n, visited);
         }
     }
 }
