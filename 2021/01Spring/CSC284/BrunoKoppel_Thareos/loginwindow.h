0ab68c85c44b322bbba703c900cc9449a97e4d8d
BrunoKoppel <bruno.koppel@gmail.com>
2021-04-08 12:49:31 -0400

    fixing bad links

--- 2021/01Spring/CSC284/BrunoKoppel_Thareos/loginwindow.h
+++ 2021/01Spring/CSC284/BrunoKoppel_Thareos/loginwindow.h
@@ -1,14 +1,35 @@
 #ifndef LOGINWINDOW_H
 #define LOGINWINDOW_H
 
-#include <QMainWindow>
 #include <QWidget>
+#include <QFile>
 
-class loginWindow : public QMainWindow
+QT_BEGIN_NAMESPACE
+namespace Ui { class loginWindow; }
+QT_END_NAMESPACE
+
+class loginWindow : public QWidget
 {
     Q_OBJECT
+
 public:
     explicit loginWindow(QWidget *parent = nullptr);
+    ~loginWindow();
+
+private slots:
+    void on_usernameLineEdit_textChanged(const QString &arg1);
+    void on_passwordLineEdit_textChanged(const QString &arg1);
+
+    void on_appLoginButton_clicked();
+    void on_appCreateAccountButton_clicked();
+
+    void on_actionExit_triggered();
+
+private:
+    Ui::loginWindow *ui;
+    void testButtonFunc();
+    void ReadAccountFromDataFile(QString username, QString password);
+    void AddAccountToDataFile(QString username, QString password, int userLevel, bool isAdmin);
 
 signals:
 
--- 2021/01Spring/CSC284/BrunoKoppel_Thareos/mainwindow.cpp
+++ 2021/01Spring/CSC284/BrunoKoppel_Thareos/mainwindow.cpp
@@ -1,5 +1,5 @@
-#include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "mainWindow.h"
+#include "ui_mainwindow.h"
 #include "user.h"
 #include <QCoreApplication>
 #include <QDebug>
