ffb9746f3f7333cf156aae8b4a62e5d01a72b3d9
Bruno Koppel <brunokoppel.dev@gmail.com>
2021-06-08 20:49:30 -0400

    adding session 01 code

---
+++ 2021/02Summer/CSC285_Advanced_Java/session_01/AccountRecord.java
@@ -0,0 +1,78 @@
+// Java core packages
+// @HW@
+// add grade average point GPA
+//
+
+import java.io.Serializable;
+
+public class AccountRecord implements Serializable { // if it is interface it
+														// should be implements
+														// (tagging interface);
+
+	private static final long serialVersionUID = 3417040053089159204L;
+	private int account;
+	private String firstName;
+	private String lastName;
+	//private String address;
+	//private String socsec;
+	//private double balance;
+	//private double gpa;
+	//private String title;
+
+	// no-argument constructor calls other constructor with
+	// default values
+	public AccountRecord() {
+		this(0, "", ""); // default way of private variables.
+	}
+
+	// initialize a record
+	public AccountRecord(int acct, String first, String last) {
+		setAccount(acct);
+		setFirstName(first);
+		setLastName(last);
+		
+	}
+
+	// set account number
+	public void setAccount(int account) {
+		this.account = account;
+	}
+
+	// get account number
+	public int getAccount() {
+		return account;
+	}
+
+	// set first name
+	public void setFirstName(String first) {
+		firstName = first;
+	}
+
+	// get first name
+	public String getFirstName() {
+		return firstName;
+	}
+
+	// set last name
+	public void setLastName(String last) {
+		lastName = last;
+	}
+
+	// get last name
+	public String getLastName() {
+		return lastName;
+	}
+	
+	
+	
+	
+} // end class AccountRecord
+
+
+
+
+
+
+
+
+
