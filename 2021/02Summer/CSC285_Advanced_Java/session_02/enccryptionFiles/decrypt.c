1f583b17a2933899849097dbe4770cbe084d3b30
BrunoKoppel <bruno.koppel@gmail.com>
2021-06-10 19:46:03 -0400

    uploading session 02 work

---
+++ 2021/02Summer/CSC285_Advanced_Java/session_02/enccryptionFiles/decrypt.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+
+int c;
+
+system("clear");
+
+while ((c=getchar()) != EOF){
+  putchar(--c);
+  // printf("%d", c);
+  }
+  
+return 1;
+}
---
+++ 2021/02Summer/CSC285_Advanced_Java/session_02/enccryptionFiles/encrypt.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+
+int c;
+
+system("clear");
+
+while ((c=getchar()) != EOF){
+  putchar(++c);
+  // printf("%d", c);
+  }
+
+return 1;
+}
+
+
